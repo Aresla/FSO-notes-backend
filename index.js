cf11cdc9021f957ff5ff1df6d8a2ccef0758b69f
Aresla <areslas@list.ru>
2023-12-13 23:05:40 +0200

    add static files from frontend project

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const unknownEndpoint = (request, response) => {
 app.use(express.json())
 app.use(requestLogger)
 app.use(cors())
+app.use(express.static('dist'))
 
 let notes = [
     {
@@ -91,7 +92,7 @@ let notes = [
 
   app.use(unknownEndpoint);
   
-  const PORT = process.env.PORT || 3008
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server is now running on port ${PORT}`)
   })
